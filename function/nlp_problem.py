c817af045e2563913c306482bde388835d8e3a5d
abilcode <m.nabilfad@gmail.com>
2023-06-07 08:38:02 +0700

    ..

--- function/nlp_problem.py
+++ function/nlp_problem.py
@@ -7,7 +7,7 @@ from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
 
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+MODEL = f"cardiffnlp/-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 # tokenizer = DistilBertTokenizerFast.from_pretrained(MODEL)
