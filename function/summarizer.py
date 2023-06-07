471079430e7fe3ce0c47eba668271d980687c665
abilcode <m.nabilfad@gmail.com>
2023-06-07 08:36:14 +0700

    .

--- function/summarizer.py
+++ function/summarizer.py
@@ -1,83 +1,83 @@
 from transformers import pipeline, AutoModel, AutoTokenizer, TFAutoModelForSeq2SeqLM, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
 import tensorflow
-import numpy as np
-import evaluate
+#import numpy as np
+#import evaluate
 
 MODEL_DIR = "Pudja2001/my_topic_summarizer_model"
 tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR)
 model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR)
 
-def preprocess_function(examples):
-    inputs = [prefix + doc for doc in examples["text"]]
-    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+# def preprocess_function(examples):
+#     inputs = [prefix + doc for doc in examples["text"]]
+#     model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
 
-    labels = tokenizer(text_target=examples["summary"], max_length=128, truncation=True)
+#     labels = tokenizer(text_target=examples["summary"], max_length=128, truncation=True)
 
-    model_inputs["labels"] = labels["input_ids"]
-    return model_inputs
+#     model_inputs["labels"] = labels["input_ids"]
+#     return model_inputs
 
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+# def compute_metrics(eval_pred):
+#     predictions, labels = eval_pred
+#     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+#     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+#     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-    result = rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+#     result = rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
 
-    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
-    result["gen_len"] = np.mean(prediction_lens)
+#     prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+#     result["gen_len"] = np.mean(prediction_lens)
 
-    return {k: round(v, 4) for k, v in result.items()}
+#     return {k: round(v, 4) for k, v in result.items()}
 
-def training_model():
-    # Use labelled datasets
-    billsum = load_dataset("billsum", split="ca_test")
+# def training_model():
+#     # Use labelled datasets
+#     billsum = load_dataset("billsum", split="ca_test")
 
-    # split train and test set
-    billsum = billsum.train_test_split(test_size=0.2)
+#     # split train and test set
+#     billsum = billsum.train_test_split(test_size=0.2)
 
-    # load pre-trained t5-small model
-    checkpoint = "t5-small"
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+#     # load pre-trained t5-small model
+#     checkpoint = "t5-small"
+#     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
-    # preprocess billsum datasets
-    prefix = "summarize: "
-    tokenized_billsum = billsum.map(preprocess_function, batched=True)
+#     # preprocess billsum datasets
+#     prefix = "summarize: "
+#     tokenized_billsum = billsum.map(preprocess_function, batched=True)
 
-    data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=checkpoint)
+#     data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=checkpoint)
 
-    # instantiate evaluate function
-    rouge = evaluate.load("rouge")
+#     # instantiate evaluate function
+#     rouge = evaluate.load("rouge")
 
-    # Define arguments to train the model
-    training_args = Seq2SeqTrainingArguments(
-    output_dir="my_topic_summarizer_model",
-    evaluation_strategy="epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    weight_decay=0.01,
-    save_total_limit=3,
-    num_train_epochs=4,
-    predict_with_generate=True,
-    fp16=False,
-    push_to_hub=True,
-    )
+#     # Define arguments to train the model
+#     training_args = Seq2SeqTrainingArguments(
+#     output_dir="my_topic_summarizer_model",
+#     evaluation_strategy="epoch",
+#     learning_rate=2e-5,
+#     per_device_train_batch_size=16,
+#     per_device_eval_batch_size=16,
+#     weight_decay=0.01,
+#     save_total_limit=3,
+#     num_train_epochs=4,
+#     predict_with_generate=True,
+#     fp16=False,
+#     push_to_hub=True,
+#     )
 
-    trainer = Seq2SeqTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_billsum["train"],
-        eval_dataset=tokenized_billsum["test"],
-        tokenizer=tokenizer,
-        data_collator=data_collator,
-        compute_metrics=compute_metrics,
-    )
+#     trainer = Seq2SeqTrainer(
+#         model=model,
+#         args=training_args,
+#         train_dataset=tokenized_billsum["train"],
+#         eval_dataset=tokenized_billsum["test"],
+#         tokenizer=tokenizer,
+#         data_collator=data_collator,
+#         compute_metrics=compute_metrics,
+#     )
 
-    trainer.train()
+#     trainer.train()
 
-    # Push the model to Huggingface Repo
-    trainer.push_to_hub()
+#     # Push the model to Huggingface Repo
+#     trainer.push_to_hub()
 
 
 def inference_model(text):
